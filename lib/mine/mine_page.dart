04fdcb4abaa8d98d057e54631b8ce3b0f4422de5
quyuanpeng <980471259@qq.com>
2022-03-15 18:56:23 +0800

    添加iOS logo 图片

---
+++ lib/mine/mine_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePageWidget extends StatefulWidget {
+  const HomePageWidget({Key? key}) : super(key: key);
+
+  @override
+  _HomePageWidgetState createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
