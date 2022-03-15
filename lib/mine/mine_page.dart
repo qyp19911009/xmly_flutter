2f9718eca255ba96014742c548139c19a2b9df0a
quyuanpeng <980471259@qq.com>
2022-03-15 19:29:55 +0800

    我的页面导航栏完成UI

--- lib/mine/mine_page.dart
+++ lib/mine/mine_page.dart
@@ -11,11 +11,28 @@ class _HomePageWidgetState extends State<HomePageWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text("我的",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.black, fontWeight: FontWeight.w600)),
-            backgroundColor: Colors.white));
+      // 顶部导航栏
+      appBar: AppBar(
+        elevation: 0.1,
+          leading: Container(width: 60,height: 20,child: Icon(Icons.email, color: Colors.black),),
+          actions: [
+            Container(width: 60,height: 20,child: Icon(Icons.settings,color: Colors.black),)
+          ],
+          title: Text("我的",
+              textAlign: TextAlign.center,
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.w600)),
+          backgroundColor: Colors.white),
+
+      /// 底部内容
+      body: ListView(
+        children: [
+          Container(
+            height: 300,
+            color: Colors.grey,
+          ),
+        ],
+      ),
+    );
   }
 }
